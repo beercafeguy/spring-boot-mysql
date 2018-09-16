a4eb030343b94eacfa86fb368500f397b72b7bc9
beercafeguy <hemchandra@outlook.com>
2018-09-16 20:50:26 +0530

    connect to my sql in spring boot in a my sql docker instance

--- src/main/java/com/github/beercafeguy/springbootmysql/SpringBootMysqlApplication.java
+++ src/main/java/com/github/beercafeguy/springbootmysql/SpringBootMysqlApplication.java
@@ -2,13 +2,23 @@ package com.github.beercafeguy.springbootmysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Import(SwaggerConfig.class)
 public class SpringBootMysqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMysqlApplication.class, args);
 	}
+
+
 }
--- src/main/java/com/github/beercafeguy/springbootmysql/UserRepository.java
+++ src/main/java/com/github/beercafeguy/springbootmysql/UserRepository.java
@@ -1,8 +1,14 @@
 package com.github.beercafeguy.springbootmysql;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.stream.Stream;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
+    /*@Query("select u from users u where u.name like '%:name%'")
+    Stream<User> findByNameReturnStream(@Param("name") String email);*/
 }
